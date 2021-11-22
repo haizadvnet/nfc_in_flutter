a291530e1e87b9117186d98bc43daef61d7f802c
Michal Černý <czernas@gmail.com>
2021-11-22 17:16:25 +0100

    fix

--- ios/Classes/NfcInFlutterPlugin.m
+++ ios/Classes/NfcInFlutterPlugin.m
@@ -68,6 +68,11 @@
     // Since this function is called when it invalidates, the session can safely be removed.
     // A new session doesn't have to be created immediately as that will happen the next time
     // startReading() is called.
+    if (session != nil) {
+        if ([session isReady]) {
+            [session invalidateSession];
+        }
+    }
     self->session = nil;
     
     // If the event stream is closed we can't send the error
