9a057f1ced09a5e349ee11449a4df7d6ae3600a1
haizadvnet <haizad@vn.my>
2024-12-03 10:28:50 +0800

    remove PluginRegistry.Registrar

--- android/src/main/java/dev/semler/nfc_in_flutter/NfcInFlutterPlugin.java
+++ android/src/main/java/dev/semler/nfc_in_flutter/NfcInFlutterPlugin.java
@@ -39,7 +39,6 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /**
  * NfcInFlutterPlugin
